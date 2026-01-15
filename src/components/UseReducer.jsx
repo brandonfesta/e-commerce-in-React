2c932f00f403d002440a42bf00bc9f2f31d69f5f
bra <brandon-f@Libero.it>
2026-01-15 22:25:10 +0100

    fatto piccolo form

--- src/components/UseReducer.jsx
+++ src/components/UseReducer.jsx
@@ -1,5 +1,30 @@
+import { useReducer, useState } from "react";
+
+function reducer(state, action){
+    switch(action.type){
+        case "input-change": {
+            return {
+                ...state,
+                input: action.eventValue
+            }
+        }
+        case "increment": {
+            return {
+                ...state,
+                age: state.age + 1
+            }
+        }
+    }
+}
+
 export default function UseReducer(){
+    const [state, dispatch] = useReducer(reducer, {input: "", age: 42})
+
     return (
-        <div></div>
+        <div>
+            <input type="text" onChange={(event) => dispatch({type: "input-change", eventValue: event.target.value})}/><br />
+            <button onClick={() => dispatch({type: "increment"})}>increment age</button>
+            <h1>Hello, {state.input}. You are {state.age}</h1>
+        </div>
     )
 }
