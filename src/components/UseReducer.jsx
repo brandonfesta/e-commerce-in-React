10be7a7d459b39bfd987762465d8636d1b481f49
bra <brandon-f@Libero.it>
2026-01-15 23:25:26 +0100

    added some func.

--- src/components/UseReducer.jsx
+++ src/components/UseReducer.jsx
@@ -1,30 +1,66 @@
-import { useReducer, useState } from "react";
+import { useReducer, useRef } from "react";
+import { nanoid } from "nanoid";
 
 function reducer(state, action){
     switch(action.type){
-        case "input-change": {
-            return {
-                ...state,
-                input: action.eventValue
-            }
+        case "aggiungi": {
+            return [
+                {
+                    text: action.input,
+                    checked: false,
+                    id: nanoid()
+                },
+                ...state
+            ]
         }
-        case "increment": {
-            return {
-                ...state,
-                age: state.age + 1
-            }
+        case "check": {
+            console.log(action.id)
+            return state.map(task => {
+                if(task.id === action.id){
+                    return {
+                        ...task,
+                        checked: !task.checked,
+                    }
+                } else{
+                    return task
+                }
+            });
         }
+        case "elimina": {
+            return state.filter(task => task.id !== action.id)
+        }
+        default:
+            throw new Error("errore")
     }
 }
 
 export default function UseReducer(){
-    const [state, dispatch] = useReducer(reducer, {input: "", age: 42})
+    const initialState = []
+
+    const inputValue = useRef()
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    let tasksLi = state.map((task) => <ListComponent task={task} dispatch={dispatch}></ListComponent>)
 
     return (
         <div>
-            <input type="text" onChange={(event) => dispatch({type: "input-change", eventValue: event.target.value})}/><br />
-            <button onClick={() => dispatch({type: "increment"})}>increment age</button>
-            <h1>Hello, {state.input}. You are {state.age}</h1>
+            <h1>Prague itinerary</h1>
+            <input type="text" ref={inputValue}/>
+            <button onClick={() => dispatch({type: "aggiungi", input: inputValue.current.value})}>Add</button>
+            <ul>
+                {tasksLi}
+            </ul>
         </div>
+    )
+}
+
+function ListComponent({task, dispatch}){
+    return (
+        <li>
+            <input type="checkbox" checked={task.checked} onChange={() => dispatch({type: "check", id: task.id})}/>
+            {task.text}
+            <button>Edit</button>
+            <button onClick={() => dispatch({type: "elimina", id: task.id})}>Delete</button>
+        </li>
     )
 }
