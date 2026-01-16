cfb7404e55ede32d77f46a83bfec92abd8c8089b
bra <brandon-f@Libero.it>
2026-01-16 16:13:04 +0100

    creata validazione form

--- src/components/UseReducer.jsx
+++ src/components/UseReducer.jsx
@@ -1,34 +1,95 @@
-import { useEffect, useReducer, useRef } from "react";
+import { useEffect, useReducer, useRef, useState } from "react"
 
-let initialState = {checked: false}
+const min_characters = 10
+const initialState = {
+    disableEmail: true,
+    disablePassword: true,
+    disableConfirm: true,
+}
+
+const CHECKS = {
+    email: "check-email",
+    password: "check-password",
+    confirmPassword: "check-confirmpassword",
+}
 
 function reducer(state, action){
     switch(action.type){
-        case "check":
-            return {
-                ...state,
-                checked: !state.checked
+        case CHECKS.email: {
+            let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            let validated = re.test(action.emailValue)
+            if(validated){
+                console.log("conf1")
+                return {
+                    ...state,
+                    disableEmail: false
+                }
+            } else{
+                return {
+                    ...state,
+                    disableEmail: true
+                }
             }
+        }
+        case CHECKS.password: {
+            if(action.passwordValue.length >= min_characters){
+                console.log("conf2")
+                return {
+                    ...state,
+                    disablePassword: false
+                }
+            } else {
+                return {
+                    ...state,
+                    disablePassword: true
+                }
+            }
+        }
+        case CHECKS.confirmPassword: {
+            if(action.confirmValue === action.passwordRef){
+                console.log("conf3")
+                return {
+                    ...state,
+                    disableConfirm: false
+                }
+            } else {
+                return {
+                    ...state,
+                    disableConfirm: true,
+                }
+            }
+        }
         default:
             throw new Error(action.type)
     }
 }
-
 export default function UseReducer(){
-
-    const [state, dispatch] = useReducer(reducer, JSON.parse(localStorage.getItem("checkToggle")) || initialState)
+    const passwordRef = useRef()
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const [disable, setDisable] = useState(true)
 
     useEffect(() => {
-        localStorage.setItem("checkToggle", JSON.stringify(state))
+        if(!state.disableEmail && !state.disablePassword && !state.disableConfirm){
+            setDisable(false)
+        } else {
+            setDisable(true)
+        }
     }, [state])
-    
 
     return (
         <div>
-            <label className="switch">
-                <input type="checkbox" onChange={() => dispatch({type: "check"})} checked={state.checked}/>
-                <span className="slider round"></span>
-            </label>
+            <form action="" method="post">
+                <label htmlFor="email">E-mail: </label>
+                <input type="email" name="email" onChange={(event) => dispatch({type: CHECKS.email, emailValue: event.target.value})}/>
+                <br />
+                <label htmlFor="password">Password: </label>
+                <input type="password" name="password" ref={passwordRef} onChange={(event) => dispatch({type: CHECKS.password, passwordValue: event.target.value})}/>
+                <br />
+                <label htmlFor="passwordConfirm">Confirm Password: </label>
+                <input type="password" name="passwordConfirm"  onChange={(event) => dispatch({type: CHECKS.confirmPassword, confirmValue: event.target.value, passwordRef: passwordRef.current.value})}/>
+                <br />
+                <input type="submit" name="submit" disabled={disable}/>
+            </form>
         </div>
     )
 }
