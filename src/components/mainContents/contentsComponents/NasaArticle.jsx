e99ec082638b18f2e53de52f30867a0adb0cdf14
bra <brandon-f@Libero.it>
2026-01-29 19:29:34 +0100

    nasa photo interactions

--- src/components/mainContents/contentsComponents/NasaArticle.jsx
+++ src/components/mainContents/contentsComponents/NasaArticle.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
 
 
 export default function NasaArticle({data}){
+   const [enlarged, setEnlarged] = useState(false)
+   function enlargeImage(){
+      if(!enlarged){
+         setEnlarged(true)
+      } else{
+         setEnlarged(false)
+      }
+   }
+
    return (
       <div className="nasa-article">
-        <img src={data.url} alt="" className="nasa-img"/>
+        <img src={data.url} alt="" className={enlarged === false? "nasa-img" : "nasa-big"} onClick={() => enlargeImage(data.title)}/>
         <h4>{data.title}</h4>
       </div>
    )
