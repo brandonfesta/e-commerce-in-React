693c231b24f8ceeb5248278841dec5a22cfa56c0
bra <brandon-f@Libero.it>
2026-02-01 21:03:59 +0100

    added some content in lateral section

--- src/components/LateralSection.jsx
+++ src/components/LateralSection.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import FooterButton from "./buttons/FooterButton"
+import { routerPaths } from "../features/header/navigationSlice"
 
 
 export default function LateralSection({setVisible, visible}){
@@ -10,6 +11,12 @@ export default function LateralSection({setVisible, visible}){
     return (
         <section className={`lateral ${visible ? "visible" : "not-visible"}`}>
         <button className="lateral-close" onClick={() => closeLateral()}>x</button>
+        <ul className="lateral-ul">
+            <li><FooterButton content={routerPaths.news}></FooterButton></li>
+            <li><FooterButton content={routerPaths.science}></FooterButton></li>
+            <li><FooterButton content={routerPaths.health}></FooterButton></li>
+            <li><FooterButton content={routerPaths.about}></FooterButton></li>
+        </ul>
         </section>
     )
 };
